c7c68fa1e0d151c08c71cdc3e2611dc4e6049f82
Michael Yu <ItzWarty@gmail.com>
2014-11-22 10:00:26 -0800

    Networking: IListenerSocket has Accept overload which takes additional cancellation token.

--- Networking/IListenerSocket.cs
+++ Networking/IListenerSocket.cs
@@ -1,10 +1,12 @@
 using System;
 using System.Net.Sockets;
+using ItzWarty.Threading;
 
 namespace ItzWarty.Networking {
    public interface IListenerSocket : IDisposable {
       Socket __Socket { get; }
 
       IConnectedSocket Accept();
+      IConnectedSocket Accept(ICancellationToken cancellationToken);
    }
 }
