69a6e90e56e6be0a5ee5d49cc3cc8a67bc192c51
Michael Yu <ItzWarty@gmail.com>
2015-02-25 16:35:43 -0800

    Add GuidProxy.

---
+++ GuidProxy.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace ItzWarty {
+   public interface GuidProxy {
+      Guid NewGuid();
+      Guid Parse(string input);
+      Guid ParseExact(string input, string format);
+      bool TryParse(string input, out Guid result);
+      bool TryParseExact(string input, string format, out Guid result);
+   }
+}
