5e51208fb6cdb77eb47b623996754a8dbe2b02a0
Michael Yu <ItzWarty@gmail.com>
2015-04-09 11:31:10 -0700

    Add stream factory methods for creating ibinaryreader/ibinarywriter.

--- IO/IStreamFactory.cs
+++ IO/IStreamFactory.cs
@@ -14,5 +14,13 @@ namespace ItzWarty.IO {
       IMemoryStream CreateMemoryStream(byte[] buffer, bool writable);
       IMemoryStream CreateMemoryStream(byte[] buffer, int index, int count);
       IMemoryStream CreateMemoryStream(byte[] buffer, int index, int count, bool writable);
+
+      IBinaryReader CreateBinaryReader(IStream stream);
+      IBinaryReader CreateBinaryReader(IStream stream, Encoding encoding);
+      IBinaryReader CreateBinaryReader(IStream stream, Encoding encoding, bool leaveOpen);
+
+      IBinaryWriter CreateBinaryWriter(IStream stream);
+      IBinaryWriter CreateBinaryWriter(IStream stream, Encoding encoding);
+      IBinaryWriter CreateBinaryWriter(IStream stream, Encoding encoding, bool leaveOpen);
    }
 }
