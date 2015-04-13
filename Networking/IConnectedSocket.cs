e7bb03d10f3326d9b1fc578946ca383f665eca6a
Michael Yu <ItzWarty@gmail.com>
2015-04-12 18:11:08 -0700

    ConnectedSocket Exposes Stream, Reader, and Writer through property.

--- Networking/IConnectedSocket.cs
+++ Networking/IConnectedSocket.cs
@@ -5,7 +5,11 @@ using System.Net.Sockets;
 namespace ItzWarty.Networking {
    public interface IConnectedSocket : IDisposable {
       Socket __Socket { get; }
+      IStream Stream { get; }
+      IBinaryReader Reader { get; }
+      IBinaryWriter Writer { get; }
 
+      IStream GetStream();
       IBinaryReader GetReader();
       IBinaryWriter GetWriter();
    }
