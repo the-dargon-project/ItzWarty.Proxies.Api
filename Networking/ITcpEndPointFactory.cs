80ba3776877aa961a17dab46be3ce29bcb0c6aa1
Warty <ItzWarty@gmail.com>
2015-08-19 02:40:21 -0700

    Expose CreateEndPoint overload that takes IPAddress over hostname.

--- Networking/ITcpEndPointFactory.cs
+++ Networking/ITcpEndPointFactory.cs
@@ -1,7 +1,10 @@
-﻿namespace ItzWarty.Networking {
+﻿using System.Net;
+
+namespace ItzWarty.Networking {
    public interface ITcpEndPointFactory {
       ITcpEndPoint CreateLoopbackEndPoint(int port);
       ITcpEndPoint CreateAnyEndPoint(int port);
       ITcpEndPoint CreateEndPoint(string host, int port);
+      ITcpEndPoint CreateEndPoint(IPAddress address, int port);
    }
 }
